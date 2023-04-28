6ca7ed97e24f120c2f47329df4214775bd4254c3
JONGMIN KIM <bellmin9321@gmail.com>
2023-04-28 16:03:27 +0900

    ✨ Feat: TodoInput 컴포넌트 구현 및 리스트 추가 기능 구현

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="inputBox shadow">
+    <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo" />
+    <button v-on:click="addTodo">+</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      newTodoItem: "",
+    };
+  },
+  methods: {
+    addTodo: function () {
+      console.log(this.newTodoItem);
+      //저장하는 로직
+      localStorage.setItem(this.newTodoItem, this.newTodoItem);
+      this.newTodoItem = ""; //비워주기(초기화)
+    },
+    clearInput: function () {
+      this.newTodoItem = ""; //비워주기(초기화)
+    },
+  },
+};
+</script>
+
+<style scoped>
+input:focus {
+  outline: none;
+}
+.inputBox {
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+}
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, #62eac6, #32cee6);
+  display: block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+}
+.addBtn {
+  color: white;
+  vertical-align: middle;
+}
+</style>
